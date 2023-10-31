23cc647f6a93900fdde91932d2609eba15b20502
NathanielMention <nathanielmention@gmail.com>
2023-10-31 09:42:04 -0400

    create backend folder

---
+++ backend/src/main/java/com/taskmanager/taskmanager/TaskmanagerController.java
@@ -0,0 +1,12 @@
+package com.taskmanager.taskmanager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskmanagerController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
