c537f917c5847c703ba55fb9d48a1ffc166cf9ec
NathanielMention <nathanielmention@gmail.com>
2023-11-01 18:58:01 -0400

    add controller folder

--- backend/src/main/java/com/taskmanager/taskmanager/controller/TaskmanagerController.java
+++ backend/src/main/java/com/taskmanager/taskmanager/controller/TaskmanagerController.java
@@ -1,4 +1,4 @@
-package com.taskmanager.taskmanager;
+package com.taskmanager.taskmanager.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
