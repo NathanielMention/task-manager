02dde158307d25a7b58f646098e0c95a930578c7
NathanielMention <nathanielmention@gmail.com>
2023-11-02 13:42:21 -0400

    edit securityconfig

--- backend/src/main/java/com/taskmanager/taskmanager/config/SecurityConfig.java
+++ backend/src/main/java/com/taskmanager/taskmanager/config/SecurityConfig.java
@@ -2,38 +2,11 @@ package com.taskmanager.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(requests -> requests
-                        .requestMatchers("/api/public/**").permitAll()
-                        .requestMatchers("/api/private/**").authenticated()
-                        .and()
-                        .formLogin(login -> login.loginPage("/login"))
-                        .logout(logout -> logout.logoutUrl("/logout"))
-                        .httpBasic(withDefaults()));
-    }
-
+public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
