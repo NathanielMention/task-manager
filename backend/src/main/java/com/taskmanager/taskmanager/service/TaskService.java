a20fb095b99d3c2996888b063d603d36aa72291e
NathanielMention <nathanielmention@gmail.com>
2023-11-01 21:16:47 -0400

    add services files/folder

---
+++ backend/src/main/java/com/taskmanager/taskmanager/service/TaskService.java
@@ -0,0 +1,41 @@
+package com.taskmanager.taskmanager.service;
+
+import com.taskmanager.taskmanager.models.Task;
+import com.taskmanager.taskmanager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task task) {
+        if (taskRepository.existsById(id)) {
+            task.setId(id);
+            return taskRepository.save(task);
+        }
+        return null; // Handle not found scenario
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
