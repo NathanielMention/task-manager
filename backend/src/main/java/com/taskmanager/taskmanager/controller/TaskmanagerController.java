cf05f2806ccf827067f302ad7f26d7e92670b969
NathanielMention <nathanielmention@gmail.com>
2023-11-01 21:40:15 -0400

    edit taskcontroller endpoints

--- backend/src/main/java/com/taskmanager/taskmanager/controller/TaskmanagerController.java
+++ backend/src/main/java/com/taskmanager/taskmanager/controller/TaskmanagerController.java
@@ -1,12 +1,43 @@
 package com.taskmanager.taskmanager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.taskmanager.taskmanager.models.*;
+import com.taskmanager.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/tasks")
 public class TaskmanagerController {
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello world";
+
+    private final TaskService taskService;
+
+    public TaskmanagerController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
     }
 }
